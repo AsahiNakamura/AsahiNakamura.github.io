23947f4e4a0f013c1e97bfa29c6478aa351e478f
asahina-dev <0.0.nn.ai@gmail.com>
2020-06-06 18:04:41 +0900

    webpackにscss関連の設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const pkg = require("./package.json");
+const styleLoaderRules = [{
+    test: /\.scss/,
+    use: [
+        "style-loader",
+        {
+            loader: "css-loader",
+            options: {
+                url: false, // CSS内のurl()メソッドの取り込みを禁止
+                importLoaders: 2
+            }
+        },
+        {
+            loader: "sass-loader"
+        }
+    ]
+}];
 module.exports = {
     mode: "development",
     devtool: "inline-source-map",
@@ -23,6 +39,6 @@ module.exports = {
         rules: [{
             test: /\.tsx?$/,
             loader: "ts-loader"
-        }]
+        }, ...styleLoaderRules]
     }
 };
