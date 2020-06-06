88757b556fb98c590b8f8edee25d854b64ce1b20
asahina-dev <0.0.nn.ai@gmail.com>
2020-06-06 17:51:12 +0900

    htmlにjsを埋め込む

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const pkg = require("./package.json");
 module.exports = {
     mode: "development",
     devtool: "inline-source-map",
@@ -9,6 +11,11 @@ module.exports = {
         filename: "scripts/[name].js",
         path: path.join(__dirname, "docs/")
     },
+    plugins: [new HtmlWebpackPlugin({
+        title: pkg.name,
+        template: path.resolve(__dirname, "./public/index.html"),
+        inject: "body"
+    })],
     resolve: {
         extensions: [".ts"]
     },
